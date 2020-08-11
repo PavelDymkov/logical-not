335c1ca7b08e5488d49d63b83e1ee9e622007da5
Pavel Dymkov <pavel.dymkov@ptnl.moscow>
2020-08-11 16:00:40 +0300

    logical-not 1.0.3

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
  * @param {any} value
  * @returns {boolean}
  */
-module.exports = function not(value) {
+function not(value) {
     return !value;
-};
+}
+
+module.exports = Object.defineProperties(not, {
+    default: { value: not },
+    __esModule: { value: true },
+});
--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 const not = require("./");
 
-
 test("true value to false", not(true) === false);
 test("false value to true", not(false) === true);
 
+test("after transpile", afterTranspileTest());
 
 function test(testName, assertion) {
-    if (assertion) console.log(`\u2713 ${testName}`);
-    else throw new Error(`\u2715 ${testName}`);
+    if (assertion) console.log(`\x1b[32m\u2713\x1b[0m ${testName}`);
+    else throw new Error(`\x1b[31m\u2715\x1b[0m ${testName}`);
+}
+
+function afterTranspileTest() {
+    "use strict";
+
+    var _ = _interopRequireDefault(require("./"));
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : { default: obj };
+    }
+
+    return (0, _.default)(true) === false;
 }
